648d0cea2d6abd6ba9f51f438fd453541fa6a589
Ricardo Santos <ricardo.eletris@gmail.com>
2024-09-12 20:29:36 -0300

    Insercao de usuario com POST

--- src/main/java/com/ricardo/workshopmongo/services/UserService.java
+++ src/main/java/com/ricardo/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ricardo.workshopmongo.domain.User;
+import com.ricardo.workshopmongo.dto.UserDTO;
 import com.ricardo.workshopmongo.repository.UserRepository;
 import com.ricardo.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,12 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
